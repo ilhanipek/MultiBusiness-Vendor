06c7a95b562ed92e1414e7b559f53201d7228bf8
İlhan İpek <aliilhanipek92@gmail.com>
2024-03-27 11:49:22 +0300

    Initial Commit

---
+++ MultiBusiness-Vendor/MultiBusiness_VendorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiBusiness_VendorApp.swift
+//  MultiBusiness-Vendor
+//
+//  Created by ilhan serhan ipek on 27.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct MultiBusiness_VendorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
